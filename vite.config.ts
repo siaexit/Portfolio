8352edb5bb5951556d358f32557699dbf24d9355
siaexit <siaexit@gmail.com>
2026-02-06 17:03:20 +0900

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc' // ← ここを plugin-react-swc に変更
 
+// vite.config.js
 export default defineConfig({
-  base: '/Portfolio/',
-  plugins: [react()],
+  base: '/Portfolio/', // リポジトリ名を / で囲んで指定
+  // ...他の設定
 })
